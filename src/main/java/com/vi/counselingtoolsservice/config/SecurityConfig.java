d10d7c2d9360e3470bc5ee6845762e72e2bb31b9
aalicic <adnan@leads.ba>
2022-12-29 18:30:34 +0100

    budibase proxy

--- src/main/java/com/vi/counselingtoolsservice/config/SecurityConfig.java
+++ src/main/java/com/vi/counselingtoolsservice/config/SecurityConfig.java
@@ -31,7 +31,7 @@ import org.springframework.security.web.csrf.CsrfFilter;
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
   public static final String[] WHITE_LIST =
-      new String[]{"/error"};
+      new String[]{"/error","budibaseproxy"};
 
   private final KeycloakClientRequestFactory keycloakClientRequestFactory;
 
